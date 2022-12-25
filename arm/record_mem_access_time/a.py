671f6d41f394a97d1babbc510858f94e1230e9c3
TaQini <742954809@qq.com>
2022-12-25 17:19:55 +0800

    hello from armv8

--- arm/record_mem_access_time/a.py
+++ arm/record_mem_access_time/a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import pandas as pd
+import matplotlib.pyplot as plt
 import sys
 
 df = pd.read_csv(sys.argv[1])
@@ -9,4 +10,11 @@ print(df.describe())
 
 print('\nnew data:')
 print(df[df<df.quantile(0.9999)].describe())
+
+print('\ndraw hist...')
+plt.hist(df[df<df.quantile(0.9999)])
+plt.xlabel('CYCLE')
+plt.ylabel('HITS')
+plt.title('CPU cycle of cache hit pre memaccess')
+plt.show()
 
