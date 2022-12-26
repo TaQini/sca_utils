f484baa14136920f5ff783887d25fa23dbc4381a
TaQini <742954809@qq.com>
2022-12-26 21:16:00 +0800

    test arm

--- arm/record_mem_access_time/run.sh
+++ arm/record_mem_access_time/run.sh
@@ -4,5 +4,3 @@ make all
 ./L1 > a.log
 ./L2 > b.log 
 
-./a.py a.log 'L1 cache hit' 1
-./a.py b.log 'L2 cache miss' 2
--- x86/record_mem_access_time_perf/run.sh
+++ x86/record_mem_access_time_perf/run.sh
@@ -5,6 +5,3 @@ make all
 ./L2 > b.log 
 ./LLC > c.log
 
-./a.py a.log 'L1 cache hit' 1 0.9999
-./a.py b.log 'L1 cache miss' 2 0.99
-./a.py c.log 'LLC miss' 3 0.95
